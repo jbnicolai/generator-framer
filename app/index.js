d968df22a6f6f38252489905e61c375c59ec597e
William Beard <wbeard@rallydev.com>
2014-07-15 15:45:01 -0600

    took away cruft

--- app/index.js
+++ app/index.js
@@ -1,13 +1,8 @@
 var yeoman = require('yeoman-generator');
 
 var FramerGenerator = yeoman.generators.Base.extend({
-  constructor: function() {
-    yeoman.generators.Base.apply(this, arguments);
-    yoInstance = this;
-  },
   framerApp: function() {
     var cb = this.async();
-    this.log.writeln('Downloading Framerjs');
     this.tarball('http://builds.framerjs.com/d14e883/Framer.zip', '.', cb);
   }
 });
